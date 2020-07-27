b1ed6b2bf84cbcd33bb317b98824138764b3607b
yuanGAO237 <yuangao270@gmail.com>
2020-07-27 14:55:43 -0700

    Update genre.js

--- genre.js
+++ genre.js
@@ -1,7 +1,7 @@
 // set the dimensions and margins of the graph
 var margin = {top: 40, right: 40, bottom: 70, left: 60},
-    width = 500 - margin.left - margin.right,
-    height = 480 - margin.top - margin.bottom;
+    width = 460 - margin.left - margin.right,
+    height = 400 - margin.top - margin.bottom;
 
 // append the svg object to the body of the page
 var svg = d3.select("#my_dataviz")
