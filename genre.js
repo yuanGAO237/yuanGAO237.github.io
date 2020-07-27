0b20af27dad82c57255c2bf6f26ae0976d3b9be8
yuanGAO237 <yuangao270@gmail.com>
2020-07-27 14:52:24 -0700

    Update genre.js

--- genre.js
+++ genre.js
@@ -1,5 +1,5 @@
 // set the dimensions and margins of the graph
-var margin = {top: 30, right: 30, bottom: 70, left: 60},
+var margin = {top: 40, right: 40, bottom: 70, left: 60},
     width = 460 - margin.left - margin.right,
     height = 400 - margin.top - margin.bottom;
 
