b6b43d5e3c56dba8db51d8f23df7910ef243c53a
yuanGAO237 <yuangao270@gmail.com>
2020-07-27 14:49:38 -0700

    Update genre.js

--- genre.js
+++ genre.js
@@ -35,11 +35,13 @@ svg.append("g")
   .call(d3.axisLeft(y));
   
 ///////newested add 1
-svg.append('g')
-   .append('text')
-   .text('count')
-
-
+var tip = d3.tip()
+  .attr('class', 'd3-tip')
+  .offset([-10, 0])
+  .html(function(d) {
+    return "<strong>Frequency:</strong> <span style='color:red'>" + d.count + "</span>";
+  })
+svg.call(tip);
 /*var tip = d3.tip()
     .attr('class', 'd3-tip')
     .offset([-10, 0])
