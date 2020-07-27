f78d528e37edf2a90024a9e9a039bd9891616c3c
yuanGAO237 <yuangao270@gmail.com>
2020-07-27 14:50:33 -0700

    Update genre.js

--- genre.js
+++ genre.js
@@ -39,7 +39,7 @@ var tip = d3.tip()
   .attr('class', 'd3-tip')
   .offset([-10, 0])
   .html(function(d) {
-    return "<strong>Frequency:</strong> <span style='color:red'>" + d.count + "</span>";
+    return "<strong>Count:</strong> <span style='color:red'>" + d.count + "</span>";
   })
 svg.call(tip);
 /*var tip = d3.tip()
