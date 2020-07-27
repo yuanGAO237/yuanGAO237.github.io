a4d36d13ca234f394674c11b7570331c43eb2f07
yuanGAO237 <30517744+yuanGAO237@users.noreply.github.com>
2020-07-26 22:42:50 -0700

    Update rating.js

--- rating.js
+++ rating.js
@@ -29,7 +29,7 @@ svg.append("g")
 
 // Add Y axis
 var y = d3.scaleLinear()
-  .domain([10000, 60000000])
+  .domain([10000, 55000000])
   .range([ height, 0]);
 svg.append("g")
   .call(d3.axisLeft(y));
