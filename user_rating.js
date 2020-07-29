a490ffde2a2a1ddd4b8e18158c87228dc56bf00f
yuanGAO237 <30517744+yuanGAO237@users.noreply.github.com>
2020-07-29 16:31:17 -0700

    Update user_rating.js

--- user_rating.js
+++ user_rating.js
@@ -13,7 +13,7 @@ var g = svg.append("g")
 var csv;
 
 // load the data
-d3.csv("user_rate.csv", function(d) {
+d3.csv("https://raw.githubusercontent.com/yuanGAO237/yuanGAO237.github.io/master/user_rate.csv", function(d) {
   d.proportion = +d.proportion;
   return d;
 }, function(error, datafile) {
